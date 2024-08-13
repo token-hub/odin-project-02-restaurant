7bed4a055a7478b46647de0c650310b88ed9362c
John <johnsuyang2118@gmail.com>
2024-08-13 21:52:13 +0800

    Minor tweak to home

--- src/home.js
+++ src/home.js
@@ -1,19 +1,17 @@
 import Display from "./display";
 
 const home = new Display("home");
-home.setHeaderTitle = "Pinoy Foods";
+home.setHeaderTitle = "Funny Pinoy Foods";
 
 const quoteDiv = home.newElement("div");
 quoteDiv.style.textAlign = "center";
-quoteDiv.style.display = "flex";
-quoteDiv.style.justifyContent = "center";
-quoteDiv.style.alignItems = "center";
 
 const restaurantQuote = home.newElement("h2");
 restaurantQuote.innerHTML = "Today's Offer!";
 
 const restaurantQuote2 = home.newElement("h3");
 restaurantQuote2.innerHTML = "Buy any 2 meal & pay for them both ...";
+
 quoteDiv.appendChild(restaurantQuote);
 quoteDiv.appendChild(restaurantQuote2);
 
