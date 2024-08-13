c7703b43d0fafa4cbe1513a3357a97c433aaa2a0
John <johnsuyang2118@gmail.com>
2024-08-13 19:21:09 +0800

    Created parent object

---
+++ src/display.js
@@ -0,0 +1,43 @@
+class Display {
+    #contentDiv = document.querySelector("#content");
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    get getName() {
+        return this.name;
+    }
+
+    get contentDiv() {
+        return this.#contentDiv;
+    }
+
+    clearContentDiv() {
+        this.contentDiv.innerHTML = "";
+    }
+
+    appendToContentDiv() {
+        this.clearContentDiv();
+        this.contentDiv.appendChild(this.header());
+        this.contentDiv.appendChild(this.content());
+    }
+
+    newElement(element = "div") {
+        return document.createElement(element);
+    }
+
+    header() {
+        const newDiv = this.newElement("div");
+        newDiv.innerHTML = "Header here!";
+        return newDiv;
+    }
+
+    content() {
+        const newDiv = this.newElement("div");
+        newDiv.innerHTML = "Content here!";
+        return newDiv;
+    }
+}
+
+export default Display;
