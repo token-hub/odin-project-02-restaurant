0ab8dc6862db205749b97b22c382ac0e79391d6a
John <johnsuyang2118@gmail.com>
2024-08-13 20:13:04 +0800

    Update index to have main class.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,38 @@ import home from "./home";
 import menu from "./menu";
 import contacts from "./contacts";
 
-let currentModule = home;
-currentModule.appendToContentDiv();
+class Main {
+    #currentModule = home;
+    constructor() {
+        this.#currentModule.appendToContentDiv();
+        this.addListeners();
+    }
 
-console.log("Hello world");
+    addListeners() {
+        const buttons = document.querySelectorAll(".nav-btn");
+        buttons.forEach((button) => {
+            button.addEventListener("click", this.changeDisplay.bind(this));
+        });
+    }
+
+    set updateCurrentModule(selectedModule) {
+        this.#currentModule = selectedModule;
+    }
+
+    get currentModule() {
+        return this.#currentModule;
+    }
+
+    changeDisplay(event) {
+        const target = event.target;
+        const id = target.id;
+
+        if (id == home.getName) this.updateCurrentModule = home;
+        if (id == menu.getName) this.updateCurrentModule = menu;
+        if (id == contacts.getName) this.updateCurrentModule = contacts;
+        this.currentModule.appendToContentDiv();
+    }
+}
+
+const restaurant = new Main();
+export default restaurant;
